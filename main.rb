2de7e4a63356ab28ef02077da9608946ffb081c3
Megurin <tutimur_0521@yahoo.co.jp>
2021-07-21 20:59:27 +0900

    class Memoとviewファイルの実装

--- main.rb
+++ main.rb
@@ -1,39 +1,73 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
+require 'pry'
+class Memo
+  def create(id:, title:, content:)
+    binding.pry
+    memo_contents = {id: id, title: title, content: content}
+    File.open('memos/#{memo_contents[:id]}.json', 'w') do |file|
+      puts JSON.generate(file)
+    end
+  end
 
-class Text
+  def self.find(id:)
+    binding.pry
+    JSON.parse(File.open('memos/#{params[:id]}.json').read, symbolize_names: true)
+  end
+
+  def update(id:, title:, content:)
+    new_contents = {id: id, title: title, content: content}
+    File.open('memos/#{new_contents[:id]}.json', 'w') do |file|
+      puts JSON.generate(file)
+    end
+  end
+
+  def destroy
+    File.delete('memos/#{params[:id]}.json')
+  end
 
 end
 
-get '/' do
-  @texts = Text.order("id desc").all
+# メモの一覧（トップページ）へのルーティング
+get '/memos' do
+  memo_list = Dir.glob('memos/*')
+  @memos = memo_list.map{|memo| JSON.parse(File.read(memo), symbolize_names: true)}
   erb :index
 end
 
-get '/show' do
-  @text = Text.find(params[:id])
-end
-
-post '/new' do
-  Text.create({:body => params[:body]})
+# メモの新規投稿へのルーティング
+get '/memos/new' do
   erb :new
 end
 
-post '/edit' do
-  @text = Text.find(params[:id])
+post '/memos/new' do
+  Memo.create(title: params[:title], content: params[:content])
+  redirect '/memos'
+  erb :new
 end
 
-post '/delete' do
-  Text.find(params[:id]).destroy
+# メモの詳細ページへのルーティング
+get '/memos/:id' do
+  @memo = Memo.find(id: params[:id])
+  erb :show
 end
 
+# メモの編集ページへのルーティング
+get '/memos/:id/edit' do
+  @memo = Memo.find(id: params[:id])
+  erb :edit
+end
 
+# メモの更新
+patch '/memos/:id' do
+  @memo = Memo.update
+  redirect '/memos'
+  erb :edit
+end
 
-
-# get '/hello/*' do |name|
-#   "hello #{name}. how are you?"
-# end
-
-# get '/erb_template_page' do
-#   erb :erb_template_page
-# end
+# メモの削除
+delete '/memos/:id' do
+  Memo.destroy
+  redirect '/memos'
+end
